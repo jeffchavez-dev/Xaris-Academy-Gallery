c94b44ce7a8c1bd0572e12e137aaa4a29289d22b
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-08-01 09:54:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ import lightGallery from 'lightgallery';
 // Plugins
 import lgThumbnail from 'lightgallery/plugins/thumbnail'
 import lgZoom from 'lightgallery/plugins/zoom'
+
+
+lightGallery(document.getElementById('lightgallery'), {
+    plugins: [lgZoom, lgThumbnail],
+    licenseKey: 'your_license_key',
+    speed: 500,
+    // ... other settings
+});
