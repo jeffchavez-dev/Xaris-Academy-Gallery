32dd3fefff6793104e2460f87c422b76d452b288
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-12-11 16:23:58 +0800

    added modal

--- script.js
+++ script.js
@@ -15,3 +15,51 @@ function reveal() {
   }
   
   window.addEventListener("scroll", reveal, {passive: true});
+
+const images = document.querySelector(".gallery_images")
+
+images.addEventListener('click', () => {
+  alert('Clicked')
+    const modal = document.createElement("div")
+    const modalImage = document.createElement("img");
+    modalImage.src = images.img;
+    modalImage.style.cursor = "pointer"
+    modal.appendChild(modalImage)
+})
+
+
+
+imageContainerGirl.forEach(image => {
+    console.log("Girl")
+    const galleryImages = document.createElement("img");
+    galleryImages.src = image.image;
+    galleryImages.style.cursor = "pointer"
+    imageContainerGirl.appendChild(galleryImages)
+
+    const modal = document.createElement("div")
+    const modalImage = document.createElement("img");
+    modalImage.src = image.image;
+    modalImage.style.cursor = "pointer"
+    modal.appendChild(modalImage)
+    
+    
+    const close = document.createElement("span")
+    close.classList.add("close")
+    close.innerText = "X"
+    modal.appendChild(close)
+
+    imageContainerGirl.addEventListener('click', () => {
+        close.style.display = "block"
+        modal.classList.add("modal")
+        modal.style.display = "flex"
+        modalImage.classList.add("modal_image")
+        imageContainerGirl.appendChild(modal)
+
+        close.addEventListener('click', () => {
+            // modal.style.display = "none";
+            modal.classList.remove("modal")
+            modal.style.display = "none"
+            close.style.display = "none"
+        })
+    })
+})
