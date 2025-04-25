16d3395a753bac1cdf4b6e69da9ebe46855a4471
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2025-04-25 13:00:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -81,3 +81,15 @@ galleryImages.forEach(image => {
   });
 
 
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach(collapsible => {
+    collapsible.addEventListener("click", function() {
+      this.classList.toggle("active");
+      const content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style
+      }
+    });
+});
