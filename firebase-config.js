886875eb08b19070c5058b1adfc1255e783559e7
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2026-06-01 15:55:23 +0800

    Add Firebase Storage upload, custom_url for static grade pages, image preview in entry modal

--- firebase-config.js
+++ firebase-config.js
@@ -11,5 +11,6 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-const db   = firebase.firestore();
-const auth = firebase.auth();
+const db      = firebase.firestore();
+const auth    = firebase.auth();
+const storage = firebase.storage();
